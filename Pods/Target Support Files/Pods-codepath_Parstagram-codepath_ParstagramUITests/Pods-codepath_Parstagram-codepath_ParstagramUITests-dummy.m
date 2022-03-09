e850d24dceae025106a5a83d588d7491c4aa4ee3
achen236 <achen236@buffalo.edu>
2022-03-08 20:35:25 -0500

    post and view images

---
+++ Pods/Target Support Files/Pods-codepath_Parstagram-codepath_ParstagramUITests/Pods-codepath_Parstagram-codepath_ParstagramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codepath_Parstagram_codepath_ParstagramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_codepath_Parstagram_codepath_ParstagramUITests
+@end
--- codepath_Parstagram/AppDelegate.swift
+++ codepath_Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "TdGtNoogEPJU192ST9sZZGLr9XW7UHEDqtKM87Vk" // <- UPDATE
+                    $0.clientKey = "lwyXY4XSK5eHUZv2OLWG095e8tSFAOo0rU1i1iqJ" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
         return true
     }
 
